08d79f2915d827f8a6ae6fed603a15b740e66814
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-02-17 13:26:28 +0100

    noPosterImg

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,28 +1,49 @@
 import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import css from './MovieList.module.css';
+import NoPosterImg from '../../images/comingSoon.jpeg';
 
 const MoviesList = ({ movies }) => {
   const location = useLocation();
 
   const elements = movies.map(({ id, title, poster_path }) => {
-    return (
-      <li key={id} className={css.li}>
-        <Link
-          to={`/movies/${id}`}
-          state={{ from: location }}
-          className={css.link}
-        >
-          <img
-            src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
-            alt={title}
-            className={css.img}
-          />
-          <p className={css.title}>{title}</p>
-        </Link>
-      </li>
-    );
+    if (poster_path) {
+      return (
+        <li key={id} className={css.li}>
+          <Link
+            to={`/movies/${id}`}
+            state={{ from: location }}
+            className={css.link}
+          >
+            <img
+              src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
+              alt={title}
+              className={css.img}
+            />
+            <p className={css.title}>{title}</p>
+          </Link>
+        </li>
+      );
+    } else {
+      return (
+        <li key={id} className={css.li}>
+          <Link
+            to={`/movies/${id}`}
+            state={{ from: location }}
+            className={css.link}
+          >
+            <img
+              src={NoPosterImg}
+              alt={title}
+              className={css.img}
+            />
+            <p className={css.title}>{title}</p>
+          </Link>
+        </li>
+      );
+    }
   });
+
   return (
     <div className={css.div}>
       <ul className={css.ul}>{elements}</ul>
--- src/components/OneMovieDetails/OneMovieDetails.jsx
+++ src/components/OneMovieDetails/OneMovieDetails.jsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import css from './OneMovieDetails.module.css';
+import NoPosterImg from '../../images/comingSoon.jpeg';
 
 const OneMovieDetails = ({ movieDetails, from }) => {
   const { title, poster_path, release_date, vote_average, overview, genres } =
@@ -17,59 +18,115 @@ const OneMovieDetails = ({ movieDetails, from }) => {
   // console.log(release_date)
   const votes = (vote_average * 10).toFixed(0);
 
-  return (
-    <>
-      {/* <h1>OneMovieDetails</h1> */}
-      <div className={css.item}>
-        <div className={css.imgBlock}>
-          <img
-            src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
-            alt={title}
-            className={css.img}
-          />
-        </div>
-        <div className={css.infoBlock}>
-          <h1 className={css.h1}>
-            {title} ({date})
-          </h1>
-          <p className={css.score}>User score: {votes}%</p>
-          <h2 className={css.h2}>Overwiev</h2>
-          <p className={css.overview}>{overview}</p>
-          <h3 className={css.h3}>Genres</h3>
-          <p className={css.genres}>{movieGenres}</p>
-          <div className={css.additional_block}>
-            <ul className={css.additional_ul}>
-              <li className={css.additional_li}>
-                <Link
-                  to="cast"
-                  state={{ from }}
-                  className={css.additional_link}
-                >
-                  <button type="button" className={css.additional_button}>
-                    Cast
-                  </button>
-                </Link>
-              </li>
-              <li className={css.additional_li}>
-                <Link
-                  to="reviews"
-                  state={{ from }}
-                  className={css.additional_link}
-                >
-                  <button type="button" className={css.additional_button}>
-                    Rewiews
-                  </button>
-                </Link>
-              </li>
-            </ul>
+  if(poster_path){
+    return (
+      <>
+        {/* <h1>OneMovieDetails</h1> */}
+        <div className={css.item}>
+          <div className={css.imgBlock}>
+            <img
+              src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
+              alt={title}
+              className={css.img}
+            />
           </div>
-          <Suspense fallback={<p>...loading</p>}>
-            <Outlet />
-          </Suspense>
+          <div className={css.infoBlock}>
+            <h1 className={css.h1}>
+              {title} ({date})
+            </h1>
+            <p className={css.score}>User score: {votes}%</p>
+            <h2 className={css.h2}>Overwiev</h2>
+            <p className={css.overview}>{overview}</p>
+            <h3 className={css.h3}>Genres</h3>
+            <p className={css.genres}>{movieGenres}</p>
+            <div className={css.additional_block}>
+              <ul className={css.additional_ul}>
+                <li className={css.additional_li}>
+                  <Link
+                    to="cast"
+                    state={{ from }}
+                    className={css.additional_link}
+                  >
+                    <button type="button" className={css.additional_button}>
+                      Cast
+                    </button>
+                  </Link>
+                </li>
+                <li className={css.additional_li}>
+                  <Link
+                    to="reviews"
+                    state={{ from }}
+                    className={css.additional_link}
+                  >
+                    <button type="button" className={css.additional_button}>
+                      Rewiews
+                    </button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <Suspense fallback={<p>...loading</p>}>
+              <Outlet />
+            </Suspense>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }else{
+    return (
+      <>
+        {/* <h1>OneMovieDetails</h1> */}
+        <div className={css.item}>
+          <div className={css.imgBlock}>
+            <img
+              src={NoPosterImg}
+              alt={title}
+              className={css.img}
+            />
+          </div>
+          <div className={css.infoBlock}>
+            <h1 className={css.h1}>
+              {title} ({date})
+            </h1>
+            <p className={css.score}>User score: {votes}%</p>
+            <h2 className={css.h2}>Overwiev</h2>
+            <p className={css.overview}>{overview}</p>
+            <h3 className={css.h3}>Genres</h3>
+            <p className={css.genres}>{movieGenres}</p>
+            <div className={css.additional_block}>
+              <ul className={css.additional_ul}>
+                <li className={css.additional_li}>
+                  <Link
+                    to="cast"
+                    state={{ from }}
+                    className={css.additional_link}
+                  >
+                    <button type="button" className={css.additional_button}>
+                      Cast
+                    </button>
+                  </Link>
+                </li>
+                <li className={css.additional_li}>
+                  <Link
+                    to="reviews"
+                    state={{ from }}
+                    className={css.additional_link}
+                  >
+                    <button type="button" className={css.additional_button}>
+                      Rewiews
+                    </button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <Suspense fallback={<p>...loading</p>}>
+              <Outlet />
+            </Suspense>
+          </div>
+        </div>
+      </>
+    );
+  }
 };
 
 export default OneMovieDetails;
